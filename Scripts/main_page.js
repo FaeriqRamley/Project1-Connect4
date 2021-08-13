f1d86439d4ce643b21b237ad7199bba2e6903922
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-08-13 17:42:30 +0800

    Added nav buttons

--- Scripts/main_page.js
+++ Scripts/main_page.js
@@ -1,1 +1,13 @@
-console.log("linked");
+"use strict";
+
+const onClickChangePage = (e) => {
+    console.log(e.target.tagName);
+    if (e.target.tagName == "BUTTON"){
+        const target = e.target.value
+        console.log("clicked");
+        window.location.assign(target);
+    }
+    
+}
+
+document.querySelector("#nav").addEventListener("click",onClickChangePage);
